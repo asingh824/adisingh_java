df11c5bc88629953b28251f3ed709d5fd4510c30
asingh824 <asingh91021@gmail.com>
2024-01-14 14:44:55 -0800

    mvntest stage added

--- src/main/java/com/minikube/sample/MinikubeSampleApplication.java
+++ src/main/java/com/minikube/sample/MinikubeSampleApplication.java
@@ -1,13 +1,16 @@
 package com.minikube.sample;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinikubeSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
 }
