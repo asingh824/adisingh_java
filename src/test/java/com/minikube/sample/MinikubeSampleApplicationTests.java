85025b3f81e8f7cf21c4c443bec1e4b9f80a351a
asingh824 <asingh91021@gmail.com>
2024-01-14 15:07:20 -0800

    mvntest stage added

--- src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
+++ src/test/java/com/minikube/sample/MinikubeSampleApplicationTests.java
@@ -1,20 +1,16 @@
 package com.minikube.sample;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MinikubeSampleApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
 }
